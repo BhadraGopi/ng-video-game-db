32cdaf14a63db4ef01517f078df60cc1a56dfcac
BhadraGopi <96776676+BhadraGopi@users.noreply.github.com>
2022-01-04 15:51:52 +0530

    done

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { AddbuttonComponent } from '../addbutton/addbutton.component';
+
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
@@ -10,16 +10,14 @@ import { AddbuttonComponent } from '../addbutton/addbutton.component';
 })
 export class SearchBarComponent implements OnInit {
   constructor(private router: Router, private dialog: MatDialog) {}
-
   ngOnInit(): void {}
   onSubmit(form: NgForm) {
     this.router.navigate(['search', form.value.search]);
   }
+  @Output() messageEmitter = new EventEmitter<string>();
+  btnClick!: string;
   showPopUp(): void {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.minWidth = 80;
-    this.dialog.open(AddbuttonComponent, dialogConfig);
+    this.btnClick = 'on';
+    this.messageEmitter.emit(this.btnClick);
   }
 }
